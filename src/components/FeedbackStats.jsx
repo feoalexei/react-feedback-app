e4cfd51949aec1806b22e133bf104f312e3b9798
feoalexei <feoalexei@ukr.net>
2023-02-02 18:44:50 +0200

    initial commit

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
+
+const FeedbackStats = () => {
+  const { feedback } = useContext(FeedbackContext);
+
+  let averageRate =
+    feedback.reduce((acc, cur) => acc + cur.rating, 0) / feedback.length;
+
+  averageRate = averageRate.toFixed(1).replace(/[.,]0$/, '');
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average rating: {isNaN(averageRate) ? 0 : averageRate}</h4>
+    </div>
+  );
+};
+
+export default FeedbackStats;
